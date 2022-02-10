17c643368f792c1d69a3aef1fd077370554c8fc5
Angelina <angelina.liapin@gmail.com>
2022-02-10 19:39:13 +0200

    formatted

--- main.js
+++ main.js
@@ -1,72 +1,73 @@
-// let c= document.querySelector('first');
+    // let c= document.querySelector('first');
+    // console.log(c.getTotalLength());
+    let game = document.querySelector('.game'),
+      res= document.querySelector('.res'),
+         btnGame = document.querySelector('.new-game'),
+            fields = document.querySelectorAll('.field'),
+                step = false,
+    circle =  `<svg class="circle">
+    <circle class="ci" r="45" cx="58" cy="58" stroke="pink"
+    stroke-width="10" fill="none" stroke-linecap="round" />
+    </svg> `,
+    cross = `<svg class="cross">
+    <line class="first" x1="15" y1="15" x2="100" y2="100" 
+    stroke="yellow" stroke-width="10"
+    stroke-linecap="round" />
+    <line class="second" x1="100" y1="15" x2="15" y2="100" 
+    stroke="red" stroke-width="10"
+    stroke-linecap="round" />
+    </svg>`;
 
-// console.log(c.getTotalLength());
-let game = document.querySelector('.game'),
-res= document.querySelector('.res'),
-btnGame = document.querySelector('.new-game'),
-fields = document.querySelector('.field'),
-step = false,
-circle= `<svg class="circle">
-<circle class="ci" r="45" cx="58" cy="58" stroke="pink"
-stroke-width="10" fill="none" 
-stroke-linecap="round" />
-</svg> `,
-cross=`<svg class="cross">
-<line class="first" x1="15" y1="15" x2="100" y2="100" 
-stroke="yellow" stroke-width="10"
-stroke-linecap="round" />
-<line class="second" x1="100" y1="15" x2="15" y2="100" 
-stroke="red" stroke-width="10"
-stroke-linecap="round" />
-</svg>`;
-
-function stepCross(target){
-target.innerHTML=cross;
-target.classList.add('x');
-let crossAudio = new Audio('audio/cross.mp3');
-crossAudio.play();
-}
-function stepZero(target){
-    target.innerHTML=circle;
-    target.classList.add('o');
-    let zeroAudio = new Audio('audio/zero.mp3');
-zeroAudio.play();
-}
-function init(e){
-if(!step) stepCross(e.target);
-else stepZero(e.target);
-step=!step;
-win();
-}
-function newGame(){
-
-}
-function win(){
-let comb = [
-[0,1,2],
-[3,4,5],
-[6,7,8],
-[0,3,6],
-[1,4,7],
-[2,5,8],
-[0,4,8],
-[2,4,6]
-];
-
-for(let i=0; i<comb.length; i++){
-
-    if(fields[comb[i][0]].classList.contains('x') &&
-    fields[comb[i][1]].classList.contains('x') &&
-    fields[comb[i][2]].classList.contains('x')){
-setTimeout(() => {
-    fields[comb[i][0]].classList.add('active');
-    fields[comb[i][1]].classList.add('active');
-    fields[comb[i][2]].classList.add('active');
-    res.innerText = 'Виграли х';
-},1500);
+    function stepCross(target) {
+        target.innerHTML=cross;
+            target.classList.add('x');
+                let crossAudio = new Audio('audio/cross.mp3');
+                    crossAudio.play();
     }
-}
 
-}
-btnGame.addEventListener('click', newGame);
-game.addEventListener('click', init);
+    function stepZero(target) {
+        target.innerHTML=circle;
+                target.classList.add('o');
+                    let zeroAudio = new Audio('audio/zero.mp3');
+                        zeroAudio.play();
+    }
+
+    function init(e) {
+        if(!step) stepCross(e.target);
+         else stepZero(e.target);
+             step=!step;
+                 win();
+    }
+
+    function newGame() {
+
+    }
+
+    function win () {
+        let comb = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+    ];
+
+    for(let i=0; i<comb.length; i++) {
+
+        if(fields[comb[i][0]].classList.contains('x') &&
+        fields[comb[i][1]].classList.contains('x') &&
+        fields[comb[i][2]].classList.contains('x')) {
+    setTimeout(() => {
+        fields[comb[i][0]].classList.add('active');
+        fields[comb[i][1]].classList.add('active');
+        fields[comb[i][2]].classList.add('active');
+        res.innerText = 'Виграли х';
+    },1500);
+             }
+         }
+    }
+    btnGame.addEventListener('click', newGame);
+    game.addEventListener('click', init);
